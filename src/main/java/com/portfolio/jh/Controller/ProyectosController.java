6417920aa69aced05516fb409606b4b57c315dcc
jhonny127 <jhonny_ore_lp@hotmail.com>
2023-05-17 01:34:20 -0300

    se actualizo

--- src/main/java/com/portfolio/jh/Controller/ProyectosController.java
+++ src/main/java/com/portfolio/jh/Controller/ProyectosController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/proyectos")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-bf8ce.web.app")
 public class ProyectosController {
    @Autowired
     ProyectosService proyectosService;
